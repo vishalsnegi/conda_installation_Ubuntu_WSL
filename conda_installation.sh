5d35fb38cc08901b51488de85f5142fbd2323376
VISHAL SINGH NEGI <62532373+vishalsnegi@users.noreply.github.com>
2021-09-14 20:46:07 +0530

    Update conda_installation.sh

--- conda_installation.sh
+++ conda_installation.sh
@@ -3,6 +3,10 @@
 ## Conda Installation for Ubuntu in Windows Subsystem for Linux (WSL)
 ## --------------------------------------------------------------- ##
 
+##----------------------------------------##
+## Download Conda
+##----------------------------------------##
+
 ## Go to https://repo.continuum.io/archive to find the list of Anaconda releases
 ## Anaconda3-5.3.1-Linux-x86_64.sh
 
@@ -10,7 +14,10 @@
 cd /mnt/e/NGS/conda_linux
 wget https://repo.continuum.io/archive/Anaconda3-5.3.1-Linux-x86_64.sh
 
-##----------------------------------------
+##----------------------------------------##
+## Install
+##----------------------------------------##
+
 ## Run the installation script: $ 
 bash Anaconda3-5.3.1-Linux-x86_64.sh
 ## installed in the location
@@ -22,12 +29,44 @@ bash Anaconda3-5.3.1-Linux-x86_64.sh
 which python
 ## It should print a path that has anaconda in it.
 
-##----------------------------------------
+##----------------------------------------##
+## Add Anaconda to the PATH
+##----------------------------------------##
+
 ## Manually add the Anaconda bin folder to your PATH. To do this, I added 
 export PATH=/home/vishal/anaconda3/bin:$PATH
 
-##----------------------------------------
+##----------------------------------------##
+## Set up Channels
+##----------------------------------------##
+
+## After installing conda you will need to add the bioconda channel 
+## as well as the other channels bioconda depends on. It is important 
+## to add them in this order so that the priority is set correctly 
+## (that is, conda-forge is highest priority).
+
+## The conda-forge channel contains many general-purpose packages 
+## not already found in the defaults channel.
+
+conda config --add channels defaults
+conda config --add channels bioconda
+conda config --add channels conda-forge
+
+##----------------------------------------##
+## Install packages
+##----------------------------------------##
+
+## Bioconda is now enabled, so any packages on 
+## the bioconda channel can be installed into the 
+## current conda environment:
+
+## Example 
+conda install bwa
+
+##----------------------------------------##
 ## Link Ubuntu Jupyter Notebook directory with my notebok drive in C drive
+##----------------------------------------##
+
 ## C:/Users/visha/Documents/Jupy_linux
 ## This directory will be used for keeping my notebook files in Windows
 cd~
@@ -37,7 +76,9 @@ ln -s /mnt/c/Users/visha/Documents/Jupy_linux
 ## you should see a notebooks folder that has everything
 ## in your C drive Jupyter Notebook Folder has
 
-##----------------------------------------
+##----------------------------------------##
+## Open Jupyter Notebook
+##----------------------------------------##
 ## To open jupyter, type 
 jupyter notebook --no-browser
 ## The no browser flag will still run Jupyter on port 8888, 
@@ -46,7 +87,8 @@ jupyter notebook --no-browser
 ## In the terminal, it will give you a link to paste into your browser. 
 ## If it worked, you should see your notebooks!
 
-##----------------------------------------
+##----------------------------------------##
 ## References
+##----------------------------------------##
+
 ## https://gist.github.com/kauffmanes/5e74916617f9993bc3479f401dfec7da
-##----------------------------------------
